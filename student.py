f1eda19614291fdb693234da20dc56b904e3eea9
robertsr <roberts7411@gmail.com>
2018-10-01 15:08:58 -0400

    adjusted wheel rotations in x_up

--- student.py
+++ student.py
@@ -59,7 +59,7 @@ class Piggy(pigo.Pigo):
     def dance(self):
         """executes a series of methods that add up to a compound dance"""
         print("\n---- LET'S DANCE ----\n")
-        ##### WRITE YOUR FIRST PROJECT HERE
+        ##### WRITE YOUR FIRST PROJECT HERE.
         self.x_up()
         self.back_step()
         self.shaky_shaky()
