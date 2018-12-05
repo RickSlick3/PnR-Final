cc23dc48f0a3024a430753e10b5f09ccea9e649d
robertsr <roberts7411@gmail.com>
2018-12-05 14:47:32 -0500

    decreased turn after chose_path to 3

--- student.py
+++ student.py
@@ -299,11 +299,11 @@ class Piggy(pigo.Pigo):
         # if right is bigger:
         if right_total > left_total:
             # turn right
-            self.encR(4)
+            self.encR(3)
         # if left is bigger:
         if left_total > right_total:
             # turn left
-            self.encL(4)
+            self.encL(3)
         return True
 
     # ADD A COUNTER TO CHOOSE_PATH if the robot turns left and right more than three times, encR(90 degrees).
