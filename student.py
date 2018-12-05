187befb21404c8dd90cde520712fdece019a1736
robertsr <roberts7411@gmail.com>
2018-12-05 15:14:12 -0500

    increased speed to L112 R120

--- student.py
+++ student.py
@@ -22,7 +22,7 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 35
         self.HARD_STOP_DIST = 25
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 111
+        self.LEFT_SPEED = 112
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.RIGHT_SPEED = 120
         # This one isn't capitalized because it changes during runtime, the others don't
